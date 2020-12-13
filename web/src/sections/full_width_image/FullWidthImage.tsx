e2cccf3e2c2e6b49da68c25a98d929a01960f765
ike76 <ike76@me.com>
2020-12-13 14:22:36 -0600

    initial starter

--- web/src/sections/full_width_image/FullWidthImage.tsx
+++ web/src/sections/full_width_image/FullWidthImage.tsx
@@ -1,12 +1,10 @@
 import { imageBuilder } from "@util/sanityImage";
-import { useViewportScroll } from "framer-motion";
 import React from "react";
 // import JTree from "../../../util/JTree";
 
 const defaultHeight = 15;
 
 const BgImage: React.FC<SectionPickerI> = ({ section, index }) => {
-  const { scrollY, scrollYProgress } = useViewportScroll();
   const heightPx = section.height * 16 || defaultHeight * 16;
   const bgUrl = imageBuilder
     .image(section.photo)
