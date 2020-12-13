7c174e85238d7bfc61032b69a14ca2c8197cc30a
ike76 <ike76@me.com>
2020-12-13 04:36:40 -0600

    fixed absolute imports jest

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,8 +1,29 @@
 module.exports = {
-  testPathIgnorePatterns: ["<rootDir> /.next/", "<rootDir> / node_modules /"],
-  setupFilesAfterEnv: ["<rootDir> /setupTests.js"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transform: {
-    "^. + \\. (js | jsx | ts | tsx) $": "<rootDir> / node_modules / babel-jest",
-    "\\. (css | less | scss | sass) $": "identity-obj-proxy",
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  roots: ["<rootDir>"],
+  moduleNameMapper: {
+    "@components/(.*)": "<rootDir>/src/components/$1",
+    "@util/(.*)": "<rootDir>/util/$1",
+    "@sections/(.*)": "<rootDir>/src/sections/$1",
+    "@sanityQueries/(.*)": "<rootDir>/sanityQueries/$1",
   },
 };
+
+function makeModuleNameMapper(srcPath, tsconfigPath) {
+  // Get paths from tsconfig
+  const { paths } = require(tsconfigPath).compilerOptions;
+
+  const aliases = {};
+
+  // Iterate over paths and convert them into moduleNameMapper format
+  Object.keys(paths).forEach((item) => {
+    const key = item.replace("/*", "/(.*)");
+    const path = paths[item][0].replace("/*", "/$1");
+    aliases[key] = srcPath + "/" + path;
+  });
+  return aliases;
+}
